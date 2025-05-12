d2f90d75e080c72cc6cfd2f94515fce8a3d24653
leonid-legit <leonid@legitsecurity.com>
2025-05-12 15:01:14 +0300

    Remove secrets Requierments added

--- .idea/app.py
+++ .idea/app.py
@@ -7,46 +7,6 @@ import simplejson as json
 
 app = Flask(__name__)
 
-# 🔐 Simulated secrets for testing secret scanning tools
-aws_access_key_id = "AKIAIOSFODNN7EXAMPLE"
-aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-
-github_token = "ghp_1234567890abcdefghijklmnopqrstuvwx"
-slack_webhook_url = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
-private_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
-
-# Stripe test key
-stripe_test_key = "sk_test_51H8G9BLoEjP8Qv6P9FAKEKEY1234567890"
-
-# Firebase test config
-firebase_config = {
-    "apiKey": "AIzaSyA-FakeKey-1234567890abcdefghijklmnop",
-    "authDomain": "fake-project.firebaseapp.com",
-    "projectId": "fake-project",
-    "storageBucket": "fake-project.appspot.com"
-}
-
-# SendGrid test API key
-sendgrid_api_key = "SG.fake_key-1234567890abcdefghijklmnop"
-
-# Mapbox token
-mapbox_token = "pk.eyJ1IjoiZmFrZXVzZXIiLCJhIjoiY2t3fWxlbW9jMjF5YzJ0bTA4cGs1bDkzZiJ9.FAKEKEY123"
-
-# Generic API token (used in many scanners)
-api_token = "api_key=12345-abcdef-67890-ghijkl"
-
-administrator_login_password = "AdminPassword123!"
-
-jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.fake.payload.signature"
-
-slack_webhook = "https://hooks.slack.com/services/T00000000/B00000000/FAKEWEBHOOK123456"
-
-
-facebook_key = "eb3692aa10723d1d3fca8a55eb78cbc9"
-fb_key = "eb3692aa10723d1d3fca8a55eb78cbc9"
-
-
-
 @app.route("/")
 def index():
     return render_template_string("Welcome to a vulnerable app!")
